21823799edb8c8bd8837d4ffc06689d079da7881
nominjinnomio20-gif <nominjinnomio20@gmail.com>
2026-05-12 20:15:09 +0800

    lab12

---
+++ Library.java
@@ -0,0 +1,40 @@
+public class Library {
+    private static Library single_instance = null;
+    private String libraryName;
+    private int totalBooks;
+    private int availableBooks;
+    private Library() {
+        libraryName = "UB Library";
+        totalBooks = 100;
+        availableBooks = 100;
+    }
+    public static Library getInstance() {
+        if (single_instance == null) {
+            single_instance = new Library();
+        }
+        return single_instance;
+    }
+    // Ном нэмэх
+    public void addBook(int amount) {
+        totalBooks += amount;
+        availableBooks += amount;
+        System.out.println(amount + "nom nemegdlee.");
+    }
+    // Ном зээлэх
+    public void borrowBook(int amount) {
+        if (amount <= availableBooks) {
+            availableBooks -= amount;
+            System.out.println(amount + " nom zeelegdlee.");
+        } else {
+            System.out.println("Hureltsengui baina. Bolomjit nom: " + availableBooks);
+        }
+    }
+    // Ном буцаах
+    public void returnBook(int amount) {
+        availableBooks += amount;
+        System.out.println(amount + " nom bucaagdlaa.");
+    }
+    public int getAvailableBooks() { return availableBooks; }
+    public int getTotalBooks()     { return totalBooks; }
+    public String getLibraryName() { return libraryName; }
+}
---
+++ Program.java
@@ -0,0 +1,18 @@
+public class Program {
+    public static void main(String[] args) {
+        Library lib1 = Library.getInstance();
+        Library lib2 = Library.getInstance();
+        if (lib1 == lib2) {
+            System.out.println("Ijil instance. Singleton pattern ajillaj baina.");
+            System.out.println("Nomiin san: " + lib1.getLibraryName());
+        }
+        System.out.println("Niit nom: " + lib1.getTotalBooks());
+        lib1.borrowBook(30);   // 100 - 30 = 70
+        lib2.borrowBook(50);   // 70  - 50 = 20
+        lib1.returnBook(10);   // 20  + 10 = 30
+        lib2.addBook(20);      // 30  + 20 = 50
+
+        System.out.println("bolomjit nom (lib1): " + lib1.getAvailableBooks());
+        System.out.println("bolomjit nom (lib2): " + lib2.getAvailableBooks());
+    }
+}
